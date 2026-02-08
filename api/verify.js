71a0c4f16a0c4b6907edd204477e1cacad9be8d1
Vthangsitink <daukhanhan34@gmail.com>
2026-02-08 20:14:44 +0700

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,13 +1,15 @@
 export default function handler(req, res) {
-    // Lệnh cấm Cache cực mạnh
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
 
     const { hwid, key } = req.query;
     if (!hwid || !key) return res.status(200).send("invalid");
 
     const secret = "BANANAVIP";
-    const timeStep = Math.floor(Date.now() / 30000); // 30 giây
+    // CHỈNH 10 GIÂY (10000ms)
+    const timeStep = Math.floor(Date.now() / 10000); 
     
     const expectedKey = "BNN_" + Buffer.from(hwid.trim() + timeStep + secret).toString('base64').substring(0, 12).toUpperCase();
 
