cc20796a8cbaf2c77682f072f080fb9aca9dbcea
Vthangsitink <daukhanhan34@gmail.com>
2026-02-08 19:48:09 +0700

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,16 +1,14 @@
 export default function handler(req, res) {
-    // Chống Cache - Ép server luôn tính toán mới
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
 
     const { hwid, key } = req.query;
     if (!hwid || !key) return res.status(200).send("invalid");
 
-    // Lấy thời gian hiện tại chính xác đến từng phút
-    const timeStep = Math.floor(Date.now() / 60000); 
+    // CHỈNH Ở ĐÂY: 30000ms = 30 giây
+    const timeStep = Math.floor(Date.now() / 30000);
     const secret = "BANANAVIP";
     
-    // Tạo key chuẩn của PHÚT HIỆN TẠI
     const expectedKey = "BNN_" + Buffer.from(hwid.trim() + timeStep + secret).toString('base64').substring(0, 12).toUpperCase();
 
     if (key.trim() === expectedKey) {
