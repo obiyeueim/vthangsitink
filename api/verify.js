92b9b5f00ea32f901b3b441889cdf402a3c0789e
Vthangsitink <daukhanhan34@gmail.com>
2026-02-08 20:00:13 +0700

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,27 +1,19 @@
 export default function handler(req, res) {
-    // ÉP SERVER KHÔNG ĐƯỢC CACHE (Quan trọng nhất)
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
     const { hwid, key } = req.query;
     if (!hwid || !key) return res.status(200).send("invalid");
 
-    const cleanHwid = hwid.trim();
-    const cleanKey = key.trim();
-    const secret = "BANANAVIP";
-
-    // Lấy thời gian hiện tại (chia 30s)
     const timeStep = Math.floor(Date.now() / 30000);
+    const secret = "BANANAVIP";
+    const expectedKey = "BNN_" + Buffer.from(hwid.trim() + timeStep + secret).toString('base64').substring(0, 12).toUpperCase();
 
-    // CHỈ TÍNH TOÁN CHO KHUNG GIỜ HIỆN TẠI
-    const expectedKey = "BNN_" + Buffer.from(cleanHwid + timeStep + secret).toString('base64').substring(0, 12).toUpperCase();
-
-    // So sánh
-    if (cleanKey === expectedKey) {
-        return res.status(200).send("valid");
+    if (key.trim() === expectedKey) {
+        res.status(200).send("valid");
     } else {
-        return res.status(200).send("invalid");
+        res.status(200).send("invalid");
     }
 }
