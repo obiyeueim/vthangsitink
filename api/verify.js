69243a249b44e1d8f46b5f36b86aa52574f30fc0
Vthangsitink <daukhanhan34@gmail.com>
2026-02-08 20:23:51 +0700

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,17 +1,14 @@
 export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
 
     const { hwid, key } = req.query;
     if (!hwid || !key) return res.status(200).send("invalid");
 
     const secret = "BANANAVIP";
-    // CHỈNH 10 GIÂY (10000ms)
-    const timeStep = Math.floor(Date.now() / 10000); 
+    const timeStep = Math.floor(Date.now() / 10000); // CHU KỲ 10 GIÂY
     
-    const expectedKey = "BNN_" + Buffer.from(hwid.trim() + timeStep + secret).toString('base64').substring(0, 12).toUpperCase();
+    const expectedKey = "BNN_" + Buffer.from(hwid.trim() + timeStep.toString() + secret).toString('base64').substring(0, 12).toUpperCase();
 
     if (key.trim() === expectedKey) {
         res.status(200).send("valid");
